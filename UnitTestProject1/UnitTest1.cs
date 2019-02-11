edaec22ffc8098a145b171db2fdf8db8b108daef
matthew payne <mdpayne26@yahoo.com>
2019-02-11 11:53:00 -0600

    just completed the add to list function

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,57 +10,70 @@ namespace UnitTestProject1
         public void TestMethod1()
         {
             //Arrange
-            CustomList<int> myList = new CustomList<int>();
-            int value = 3;
-            int expectedResult = 1;
-            //Act
-            myList.Add(value);
+            CustomList
+            
+            
+            
+            
+            
+        //    //Arrange
+        //    CustomList<int> myList = new CustomList<int>();
+        //    int value = 3;
+        //    int expectedResult = 1;
+        //    //Act
+        //    myList.Add(value);
 
-            //Assert
-            Assert.AreEqual(expectedResult, myList.Count);
-        }
-        [TestMethod]
-        public void TestMethod2()
-        {
-            //Arrange
-            CustomList<int> myList = new CustomList<int>();
-            int value = 3;
-            int expectedResult = 3;
-            //Act
-            myList.Add(value);
+        //    //Assert
+        //    Assert.AreEqual(expectedResult, myList.Count);
+        //}
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //    //Arrange
+        //    CustomList<int> myList = new CustomList<int>();
+        //    int value = 3;
+        //    int expectedResult = 3;
+        //    //Act
+        //    myList.Add(value);
 
-            //Assert
-            Assert.AreEqual(expectedResult, myList[0]);
-        }
-        [TestMethod]
-        public void TestMethod3()
-        {
-            //Arrange
-            CustomList<int> myList = new CustomList<int>();
-            int value = 3;
-            int value1 = 5;
-            int expectedResult = 3;
-            //Act
-            myList.Add(value);
-            myList.Add(value1);
+        //    //Assert
+        //    Assert.AreEqual(expectedResult, myList[0]);
+        //}
+        //[TestMethod]
+        //public void TestMethod3()
+        //{
+        //    //Arrange
+        //    CustomList<int> myList = new CustomList<int>();
+        //    int value = 3;
+        //    int value1 = 5;
+        //    int expectedResult = 3;
+        //    //Act
+        //    myList.Add(value);
+        //    myList.Add(value1);
 
-            //Assert
-            Assert.AreEqual(expectedResult, myList[0]);
-        }
-        [TestMethod]
-        public void TestMethod4()
-        {
-            //Arrange
-            CustomList<int> myList = new CustomList<int>();
-            int value = 3;
-            int value1 = 5;
-            int expectedResult = 5;
-            //Act
-            myList.Add(value);
-            myList.Add(value1);
+        //    //Assert
+        //    Assert.AreEqual(expectedResult, myList[0]);
+        //}
+        //[TestMethod]
+        //public void TestMethod4()
+        //{
+        //    //Arrange
+        //    CustomList<int> myList = new CustomList<int>();
+        //    int value = 3;
+        //    int value1 = 5;
+        //    int expectedResult = 5;
+        //    //Act
+        //    myList.Add(value);
+        //    myList.Add(value1);
 
-            //Assert
-            Assert.AreEqual(expectedResult, myList[1]);
+        //    //Assert
+        //    Assert.AreEqual(expectedResult, myList[1]);
+        //}
+        //[TestMethod]
+        //public void TestMethod5()
+        //{
+        //    //Arrange
+        //    CustomList
         }
     }
 }
